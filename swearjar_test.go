8b64da96712598d8468c450068ca359d9e7a01f0
Scott Nicol <s.cott@me.com>
2017-07-28 13:40:10 +0100

    Uses regex rather than substr in match, refactor so Load() returns a Swears instance and include more tests

--- swearjar_test.go
+++ swearjar_test.go
@@ -6,32 +6,71 @@ import (
 )
 
 func TestLoadSwears(t *testing.T) {
-	swears, err := loadSwears()
+	swears, err := Load()
 
 	if err != nil {
 		t.Error(err)
 	}
 
 	if swears == nil {
-		t.Error("Swears is nil")
+		t.Error("Swears could not be loaded")
+	}
+}
+
+func TestLoadSwearsWithNonexistentFile(t *testing.T) {
+	swears, err := Load("nonexistent.json")
+
+	if err == nil {
+		t.Error(err)
 	}
 
+	if swears != nil {
+		t.Error("Swears could be loaded when they should not")
+	}
+}
+
+func TestLoadSwearsWithBadJSON(t *testing.T) {
+	swears, err := Load("swearjar_test_bad.json")
+
+	if err == nil {
+		t.Error(err)
+	}
+
+	if swears != nil {
+		t.Error("Swears could be loaded when they should not")
+	}
 }
 
 func TestProfane(t *testing.T) {
-	profane, err := Profane("fuck is a word")
-
-	if profane == false {
-		t.Error("fuck is profane")
-	}
+	swears, err := Load()
 
 	if err != nil {
 		t.Error(err)
 	}
+
+	profane, err := swears.Profane("fuck is a word")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if profane == false {
+		t.Error("fuck is profane")
+	}
 }
 
 func TestScorecard(t *testing.T) {
-	profane, reasons, err := Scorecard("what an asslick")
+	swears, err := Load()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	profane, reasons, err := swears.Scorecard("what an asslick")
+
+	if err != nil {
+		t.Error(err)
+	}
 
 	if profane == false {
 		t.Error("asslick is profane")
@@ -41,8 +80,26 @@ func TestScorecard(t *testing.T) {
 	if !reflect.DeepEqual(reasons, expectedReasons) {
 		t.Error("does not match reason")
 	}
+}
+
+func TestScorecardNoMatch(t *testing.T) {
+	swears, err := Load()
 
 	if err != nil {
 		t.Error(err)
+	}
+
+	profane, reasons, err := swears.Scorecard("this is a lovfuckely sentshitence with no foul languassge")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if profane == true {
+		t.Error("this is not profane")
+	}
+
+	if len(reasons) > 0 {
+		t.Error("should not have a reason")
 	}
 }
